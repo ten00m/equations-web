b4bf9270e1578d35ec600b969cf5bb01f8daed0d
ten00m <ilya.safronov.1989@gmail.com>
2023-03-21 04:06:29 +0500

    'simplifyer'

--- src/models/utils/Simplifyer.ts
+++ src/models/utils/Simplifyer.ts
@@ -2,18 +2,32 @@ import {fraction, format, parse, rationalize, simplify} from 'mathjs'
 
 
 export class Simplifyer {
+
 	public static evaluate(root: any){
-		const node = parse('sqrt(145)');
+		root = root.transform((node: any) => {
+			if(node?.name === 'sqrt' && node.isFunctionNode){
+				const expr = rationalize(node.args[0])
+				const sqrt = parse(`sqrt(${expr})`)
+				if(this.checkIrrational(sqrt)){
+					return this.partialExt(sqrt)
+				}
+			}
 
-		node.traverse((node: any) => {
-			if(node?.name === 'sqrt' && this.checkIrrational(node)){
-				console.log('!!!')
-			}	
+			return node
 		})
-	}	
 
-	// надо написать взаимодейтсвие с десятичныеми дробями домножаем до целого если количетсво знаков после запятой четное
-	// принимает только fraction
+		console.log(root.toString())
+
+		const [transformed, sqrtExps] = this.replace(root);
+
+		let simplified = simplify(transformed);
+		console.log(transformed.toString())
+
+		simplified = this.revReplace(simplified, sqrtExps)
+
+
+		console.log(simplified.toString())
+	}	
 	private static checkPeriodic(fract: any): boolean{
 		let m = fract.d;
 
@@ -38,7 +52,6 @@ export class Simplifyer {
 
 			for(let el of pri){
 
-				console.log(pri, el )
 				if((pri.lastIndexOf(el) + 1) % 2 !== 0){
 					isIrrational = true
 				}
@@ -60,5 +73,77 @@ export class Simplifyer {
 		}
 
 		return primers
+	}
+
+	private static replace(root: any): any{
+		const sqrtExps: Array<number> = [] //массив с подкоренными выражениями, выражения соответствуют y^n где n = индекс данного выражения + 1
+		let n = 0
+		
+		const transformed = root.transform((node: any) => {
+			if(node?.name === 'sqrt' && node.isFunctionNode){
+				let sqrtExp = node.args[0];
+				sqrtExp = rationalize(sqrtExp);
+				sqrtExp = sqrtExp?.value ? sqrtExp?.value : 0
+				if(this.checkIrrational(node)){
+					if(sqrtExps.indexOf(sqrtExp) === -1){
+						sqrtExps.push(sqrtExp);
+					}
+
+					n = sqrtExps.indexOf(sqrtExp) + 2
+
+					return parse(`y^${n}`)
+				}
+			}
+
+			return node
+		})
+
+		return [transformed, sqrtExps]
+	}
+
+	// возвращает частично извлеченный корень 
+	private static partialExt(sqrt: any): any{
+		let mod = 1;
+		let mult = 1
+		let sqrtExp: any = rationalize(sqrt.args[0]);
+		sqrtExp = sqrtExp?.value ? sqrtExp?.value : 0;
+		let primersOfExp = this.getPrimers(sqrtExp);
+
+		for(let el of primersOfExp){
+			const lastInd = primersOfExp.lastIndexOf(el);
+			if(lastInd + 1 === primersOfExp.length){
+
+			}
+
+			if((lastInd + 1) % 2 !== 0){
+				mod *= el;
+				mult *= el**(lastInd / 2)
+			} else {
+				mult *= el**((lastInd + 1) / 2)
+			}
+
+			if(lastInd + 1 === primersOfExp.length){
+				break
+			}
+			const ind = -(primersOfExp.length - lastInd) + 1
+			primersOfExp = primersOfExp.slice(ind)
+		}
+
+		return parse(`${mult}*sqrt(${mod})`)
+	}
+
+	private static revReplace(node: any, sqrtExps: Array<number>): any{
+		const transformed = node.transform((node: any) => {
+			if(node?.op === '^'){
+				const n = node.args[1]?.value - 2
+				const sqrtExp = sqrtExps[n]
+
+				return parse(`sqrt(${sqrtExp})`)
+			}
+
+			return node
+		})
+
+		return transformed
 	}
 }
